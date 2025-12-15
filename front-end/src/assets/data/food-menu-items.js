711e0b48f2dcfdde0a47632ba2ab771459f85a80
kazungu-p <patrickkazungu@icloud.com>
2025-12-15 14:30:24 +0300

    add menu for variety of dishes for user to select from

---
+++ front-end/src/assets/data/food-menu-items.js
@@ -0,0 +1,44 @@
+const food_menu_items = [
+    {
+        key: 1,
+        item_image:"/images/pizza.jpg",
+        menu_name:"pizza"
+    },
+    {
+        key: 2,
+        item_image:"/images/cheese.jpg",
+        menu_name:"cheese"
+    },
+    {
+        key: 3,
+        item_image:"/images/salad.jpg",
+        menu_name:"salad"
+    },
+    {
+        key: 4,
+        item_image:"/images/chicken.jpg",
+        menu_name:"chicken"
+    },
+    {
+        key: 5,
+        item_image:"/images/burger.jpg",
+        menu_name:"burger"
+    },
+    {
+        key: 6,
+        item_image:"/images/biryani.jpg",
+        menu_name:"biryani"
+    },
+    {
+        key: 7,
+        item_image:"/images/toast.jpg",
+        menu_name:"toast"
+    },
+    {
+        key: 8,
+        item_image:"/images/pasta.jpg",
+        menu_name:"pasta"
+    }
+]
+
+export default food_menu_items;
--- front-end/src/components/FoodMenu.jsx
+++ front-end/src/components/FoodMenu.jsx
@@ -1,6 +1,26 @@
 import React from "react";
+import "../assets/styles/foodmenu.css"
+import food_menu_items from "../assets/data/food-menu-items";
 
 function FoodMenu(){
+    return(
+        <div className="food-menu-container">
+            <h2>Explore our menu</h2>
+            <p className="food-menu-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates in modi asperiores suscipit et sequi necessitatibus recusandae veritatis eveniet omnis.</p>
+            <div className="food-menu-list">
+                {food_menu_items.map((item, key)=>{
+                    return(
+                        <div key={key} className="food-menu-list-item">
+                            <img src={item.item_image} alt="" />
+                            <p>{item.menu_name}</p>
+                        </div>
+                    )
+                })}
+            </div>
+            <hr />
+        </div>
+
+    )
 
 }
 export default FoodMenu;
