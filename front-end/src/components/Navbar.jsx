45de0601ccf59e5e58a8e4fd36430f025ff57666
kazungu-p <patrickkazungu@icloud.com>
2025-12-17 15:41:44 +0300

    add navbar menu for small screens with smooth trsnasition for open and close

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import "../assets/styles/navbar.css"
 
-function Navbar(){
+function Navbar() {
 
     const [menuOpen, setMenuOpen] = useState(false);
-    function toggleMenu(){
+    function toggleMenu() {
         setMenuOpen(!menuOpen);
     }
+    function closeMenu(){
+        setMenuOpen(false);
+    }
 
-    return(
+    return (
         <header>
             <nav>
                 <div className="navbar">
                     <h1>Kibandaski.</h1>
-                    <ul className="nav-links">
-                        <li><NavLink to="/" className={({isActive})=> isActive? "nav-item active": "nav-item"}>Home</NavLink></li>
-                        <li><NavLink to="/menu" className={({isActive})=> isActive? "nav-item active": "nav-item"}>Menu</NavLink></li>
-                        <li><NavLink to="/mobile-app" className={({isActive})=> isActive? "nav-item active": "nav-item"}>Mobile App</NavLink></li>
-                        <li><NavLink to="/contact-us" className={({isActive})=> isActive? "nav-item active": "nav-item"}>Contact Us</NavLink></li>
+                    <ul className={`nav-links ${menuOpen? "active": ""}`}>
+                        <li><NavLink to="/" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"} onClick={closeMenu}>Home</NavLink></li>
+                        <li><NavLink to="/menu" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"} onClick={closeMenu}>Menu</NavLink></li>
+                        <li><NavLink to="/mobile-app" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"} onClick={closeMenu}>Mobile App</NavLink></li>
+                        <li><NavLink to="/contact-us" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"} onClick={closeMenu}>Contact Us</NavLink></li>
                     </ul>
                     <div className="nav-right">
                         <img src="/images/magnifying-glass-solid-full.svg" alt="magnifying glass image" />
@@ -27,10 +30,11 @@ function Navbar(){
                             <div className="float"></div>
                         </div>
                         <button>Sign in</button>
+                        <div className="hamburger" onClick={toggleMenu}>
+                            {menuOpen ? "\u2715" : "\u2630"}
+                        </div>
                     </div>
-                    <div className="hamburger" onClick={toggleMenu}>
-                        {menuOpen? "\u2615": "\u2630"}
-                    </div>
+
                 </div>
             </nav>
         </header>
